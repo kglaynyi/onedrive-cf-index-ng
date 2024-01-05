d1e06ef594faa2383667a264426c5487f5ab3820
kglaynyi <94588643+kglaynyi@users.noreply.github.com>
2024-01-05 16:02:55 +0630

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   title: "NLIN",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
-  baseDirectory: '/',
+  baseDirectory: '/NLIN',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -40,7 +40,7 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   // This is the path relative to `baseDirectory` above, not relative to root in your Onedrive.
-  protectedRoutes: ['/'],
+  protectedRoutes: ['/NLIN'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "",
